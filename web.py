37eaa8a28130cbc4b483bc40515c9b19b7b6076f
Mathew <mathew@winters.org.nz>
2019-10-12 11:28:39 +1300

    Add platform IO as build system.

--- web.py
+++ web.py
@@ -1,8 +1,19 @@
+#!/bin/python
+
+# from SCons.Script import DefaultEnvironment
+# env = DefaultEnvironment()
+
 Import("env")
+
+# # access to global build environment
+# print(env)
 
 
 def before_spiffs(source, target, env):
-  print("XXXXXX before_spiffs")
+  env.Execute("./build-web-app.sh")
 
-env.AddPreAction("buildfs", before_spiffs)
-print("XXXXX script")
+# env.AddPreAction("buildfs", before_spiffs)
+env.AddPreAction(".pio/build/%s/spiffs.bin" % env['PIOENV'], before_spiffs)
+# env.AddPreAction(".pio/build/pre_and_post_hooks/spiffs.bin", before_spiffs)
+
+# print("XXXXX script %s" % env['PIOENV'])
